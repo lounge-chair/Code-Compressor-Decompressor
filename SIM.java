19d2c1f5c8e52d65bf7816f411d1ca1939365e57
Danny <dannydftw@gmail.com>
2020-03-28 20:51:17 -0400

    dictionary implemented, a little bit of debug code left in

--- SIM.java
+++ SIM.java
@@ -1,36 +1,92 @@
 // On my honor, I have neither given nor received unauthorized aid on this assignment.
 
 import java.util.*;
-
-import javax.lang.model.util.ElementScanner6;
-
 import java.io.*;
+import java.util.Map.Entry;
 
 public class SIM {
-    // Declare IO files
-    // TODO: split this up for files for compression and decompression
     public static void main(String[] args) throws FileNotFoundException{
         // Compression
-        if (args[0].equals("1")) {
+        //if (args[0].equals("1")) {
             compression();
-        }
+        //}
         // Decompression
-        else if (args[0].equals("2")) {
+        //else if (args[0].equals("2")) {
             // decompression();
-        } else {
-            System.out.println("Error! Invalid command entered. Please try again!");
-        }
+        //} else {
+        //    System.out.println("Error! Invalid command entered. Please try again!");
+        //}
     }
 
     public static void compression(){
+        Vector<String> instructions = new Vector<String>();
+        String[] dictionary = new String[8];
+        Map<String, Integer> countMap = new LinkedHashMap<>();
         // Set up I/O
-        try{
-        File original = new File("original.txt");
-        PrintStream cout = new PrintStream("cout.txt");
-        System.setOut(cout);
-        } catch(FileNotFoundException e) {
+        try {
+            // Input
+            File original = new File("original.txt");
+            Scanner scan = new Scanner(original);
+            // Output
+            PrintStream cout = new PrintStream("cout.txt");
+            System.setOut(cout);
+            
+            // Add binary to instruction vector
+            while (scan.hasNextLine()) {
+                instructions.add(scan.nextLine());
+            }
+            scan.close();
+            //DEBUG: print out instruction vector
+            for (int i = 0; i < instructions.size(); i++) {
+                System.out.println(instructions.get(i));
+            }
+            System.out.println("xxxx");
+            //
+
+            // Dictionary
+            for (int i = 0; i < instructions.size(); i++) {
+                if(countMap.containsKey(instructions.get(i))){
+                    countMap.put(instructions.get(i), countMap.get(instructions.get(i))+1);
+                }
+                else {
+                    countMap.put(instructions.get(i), 1);
+                }
+            }
+
+            ArrayList<Entry<String, Integer>> fullDictionary = new ArrayList<>(countMap.entrySet());
+
+            Collections.sort(fullDictionary, new Comparator<Entry<String, Integer>>()
+                {
+                    @Override
+                    public int compare(Entry<String, Integer> e1, Entry<String, Integer> e2)
+                        {
+                            if(e1.getValue() != null && e2.getValue() != null) {
+                                return e2.getValue().compareTo(e1.getValue());
+                            }
+                            else {
+                                System.out.println("Error! Null value in map.");
+                                return -1;
+                            }
+                        }
+                } 
+            );
+            
+            // for(Entry<String, Integer> entry: entries)
+            // {
+            //     System.out.println(entry.getKey());
+            // }
+
+            for(int i = 0; i < 8; i++)
+            {
+                //DEBUG: 
+                System.out.println(fullDictionary.get(i).getKey());
+                System.out.println("writing to array...");
+                //
+                dictionary[i] = fullDictionary.get(i).getKey();
+            }
+
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        System.out.println("Test!");
     }
 }
